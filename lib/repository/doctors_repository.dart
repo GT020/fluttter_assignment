123d0b0561f0cb940ab4de537b979e33f2fe07a2
GT020 <gauravtanwar0020@gmail.com>
2022-06-25 17:51:18 +0530

    repository: add functions to save and retreive data from local storage

--- lib/repository/doctors_repository.dart
+++ lib/repository/doctors_repository.dart
@@ -1,24 +1,46 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:bima_doctor_admin/api_provider/doctor_api.dart';
 import 'package:bima_doctor_admin/model/doctor.dart';
+import 'package:hive/hive.dart';
 
 abstract class DoctorsRepository {
   Future<List<Doctor>> getAllDoctors();
 }
 
 class DoctorsRepositoryImpl implements DoctorsRepository {
-  late bool isDataFetchedFromApi;
+  late final Box doctorHiveBox;
   late DoctorProviderApi _doctorProviderApi;
   DoctorsRepositoryImpl() {
     _doctorProviderApi = DoctorProviderApiImpl();
+    doctorHiveBox = Hive.box('doctors');
   }
   @override
   Future<List<Doctor>> getAllDoctors() async {
     final List<dynamic> doctorsJsonList =
         await _doctorProviderApi.getDoctorList();
-    return doctorsJsonList.map((dynamic doctorJson) {
+
+    List<Doctor> doctors = doctorsJsonList.map((dynamic doctorJson) {
       return Doctor.fromJson(doctorJson);
     }).toList();
+    return doctors; 
+  }
+
+  Future<void> savedEditedDoctor(Doctor doctor) async {
+    try {
+      doctorHiveBox.put(doctor.id, doctor);
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<List<Doctor>> getSavedDoctors(int id) async {
+    final int length = doctorHiveBox.length;
+    final List<Doctor> doctors = [];
+    for (int i = 0; i < length; i++) {
+      doctors.add(doctorHiveBox.getAt(i));
+    }
+    return doctors;
   }
 }
