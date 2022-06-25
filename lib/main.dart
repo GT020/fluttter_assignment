a35151d10ddb05839659c51c0de4c691aff6c8a7
GT020 <gauravtanwar0020@gmail.com>
2022-06-25 17:53:38 +0530

    main: sync after adding navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:bima_doctor_admin/app/route_constants.dart';
+import 'package:bima_doctor_admin/app/route_generator.dart';
+import 'package:bima_doctor_admin/model/doctor.dart';
 import 'package:bima_doctor_admin/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(DoctorAdapter());
+  await Hive.openBox('doctors');
   runApp(const ProviderScope(child: DoctorAdminApp()));
 }
 
@@ -11,6 +18,9 @@ class DoctorAdminApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Doctor Admin', home: HomeView());
+    return const MaterialApp(
+        onGenerateRoute: RouteGenerator.generateRoute,
+        title: 'Doctor Admin',
+        home: HomeView());
   }
 }
